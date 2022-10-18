e036762852235d783f05ed9e7d2da80ea1f6bdf9
Iconart <okequadri43@gmail.com>
2022-10-18 21:22:34 +0000

    function to print the integer value

---
+++ integer_operation.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * op_integer - function to get the integer value
+ * @argz: the name of the list argument
+ * Return: the end result of digit count
+ */
+
+int op_integer(argz)
+{
+	int digit_count;
+
+	digit_count = int d_i_print(argz);
+	return (digit_count);
+}
+/**
+ * d_i_print - function to print intergers
+ * @argz: the argument that is being passed to the argument
+ * Return: count
+ */
+
+int d_i_print(argz)
+{
+	int digit, count, temp;
+	int place_holder, whole;
+
+	place_holder = 1;
+	whole = count = temp = digit = 0;
+	digit = va_arg(argz, int);
+
+	if (digit < 0)
+	{
+		_putchar(48);
+		count++;
+		return (count);
+	}
+
+	temp = digit;
+	while (temp > 9)
+	{
+		place_holder = div * 10;
+		temp = temp / place_holder;
+	}
+	while (place_holder > 0)
+	{
+		whole = digit / place_holder;
+		_putchar(whole + 48);
+		count++;
+		digit = digit % place_holder;
+		placeholder = placeholder / 10;
+	}
+	return (count);
+}
