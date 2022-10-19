152721c923dd2ed78e18eebdb62cdbcaa079dada
Yusuf-R <y.abdulwasiu@gmail.com>
2022-10-19 15:36:08 +0100

    Fixed Error and added unsigned function

--- integer_operation.c
+++ integer_operation.c
@@ -63,10 +63,9 @@ int d_i_print(va_list argz)
 		tmp1 = temp;
 	}
 
-	while (tmp1 > 9)
+	while ((tmp1 / place_holder) > 9)
 	{
 		place_holder = place_holder * 10;
-		tmp1 /= place_holder;
 	}
 
 	while (place_holder != 0)
